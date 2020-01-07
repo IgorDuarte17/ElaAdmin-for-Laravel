e15108df34080a51bccfe7156fab4f9f57176d98
Igor Santos <igors.d@hotmail.com>
2020-01-06 21:26:30 -0300

    Added components routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ Route::get('/', function () {
 });
 
 Route::get('/home',  'HomeController@index')->name('home');
+
+
+// Simulação
+Route::prefix('/components')->group(function ()
+{
+    Route::get('ui-buttons', 'ComponentsController@buttons')->name('ui_buttons');
+    Route::get('ui-badges', 'ComponentsController@badges')->name('ui_badges');
+    Route::get('ui-tabs', 'ComponentsController@tabs')->name('ui_tabs');
+    Route::get('ui-cards', 'ComponentsController@cards')->name('ui_cards');
+    Route::get('ui-alerts', 'ComponentsController@alerts')->name('ui_alerts');
+    Route::get('ui-progressbar', 'ComponentsController@progressbar')->name('ui_progressbar');
+    Route::get('ui-modals', 'ComponentsController@modals')->name('ui_modals');
+    Route::get('ui-switches', 'ComponentsController@switches')->name('ui_switches');
+    Route::get('ui-grids', 'ComponentsController@grids')->name('ui_grids');
+    Route::get('ui-typgraphy', 'ComponentsController@typgraphy')->name('ui_typgraphy');
+});
